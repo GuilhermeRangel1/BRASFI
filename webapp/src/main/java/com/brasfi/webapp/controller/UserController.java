78d8abf75c973a74fca919b84b851a0629105332
GuilhermeRangelSilva <162377684+GuilhermeRangel1@users.noreply.github.com>
2025-05-31 17:50:50 -0300

    update que quebrou antes

--- webapp/src/main/java/com/brasfi/webapp/controller/UserController.java
+++ webapp/src/main/java/com/brasfi/webapp/controller/UserController.java
@@ -13,7 +13,11 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
-
+    
+    @GetMapping("/")
+    public String home() {
+        return "inicio"; 
+    }
     @GetMapping("/login")
     public String login() {
         return "login"; 
